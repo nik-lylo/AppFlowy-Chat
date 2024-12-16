1597091ef2be2a782d7c53b91f30a60a23ae41b1
Nick Lylo <lulnuk1995@gmail.com>
2024-12-16 22:25:16 +0200

    fix:/tests/ path updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,58 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./lib/**/*.{js,ts,jsx,tsx}", "./test/**/*.{js,ts,jsx,tsx}"],
+  content: ['./lib/**/*.{js,ts,jsx,tsx}', './tests/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       spacing: {
-        "3e": "0.875rem",
-        "4e": "1.125rem",
-        "5e": "1.375rem",
-        "7e": "1.875rem",
-        13: "3.25rem",
-        15: "3.75rem",
-        18: "4.5rem",
-        30: "7.5rem",
+        '3e': '0.875rem',
+        '4e': '1.125rem',
+        '5e': '1.375rem',
+        '7e': '1.875rem',
+        13: '3.25rem',
+        15: '3.75rem',
+        18: '4.5rem',
+        30: '7.5rem',
       },
       colors: {
-        "ch-bg": {
-          base: "var(--chat-bg-base)",
+        'ch-bg': {
+          base: 'var(--chat-bg-base)',
         },
 
-        "ch-text": {
-          title: "var(--chat-text-title)",
-          caption: "var(--chat-text-caption)",
-          content: "var(--chat-text-content)",
-          placeholder: "var(--chat-text-placeholder)",
-          disabled: "var(--chat-text-disabled)",
+        'ch-text': {
+          title: 'var(--chat-text-title)',
+          caption: 'var(--chat-text-caption)',
+          content: 'var(--chat-text-content)',
+          placeholder: 'var(--chat-text-placeholder)',
+          disabled: 'var(--chat-text-disabled)',
         },
-        "ch-accent": {
-          DEFAULT: "var(--content-blue-400)",
+        'ch-accent': {
+          DEFAULT: 'var(--content-blue-400)',
         },
-        "ch-primary": {
-          dark2: "var(--chat-primary-dark2)",
-          gray: "var(--chat-primary-gray)",
-          gray2: "var(--chat-primary-gray2)",
+        'ch-primary': {
+          dark2: 'var(--chat-primary-dark2)',
+          gray: 'var(--chat-primary-gray)',
+          gray2: 'var(--chat-primary-gray2)',
         },
-        "ch-line": {
-          border: "var(--chat-line-border)",
-          divider: "var(--chat-line-divider)",
+        'ch-line': {
+          border: 'var(--chat-line-border)',
+          divider: 'var(--chat-line-divider)',
         },
-        "ch-icon": {
-          secondary: "var(--chat-icon-secondary)",
-          blue: "var(--chat-icon-blue)",
+        'ch-icon': {
+          secondary: 'var(--chat-icon-secondary)',
+          blue: 'var(--chat-icon-blue)',
         },
-        "ch-fill": {
-          active: "var(--fill-list-active)",
-          hover: "var(--fill-list-hover)",
-          "card-user": "var(--chat-card-user)",
-          tooltip: "var(--chat-tooltip)",
+        'ch-fill': {
+          active: 'var(--fill-list-active)',
+          hover: 'var(--fill-list-hover)',
+          'card-user': 'var(--chat-card-user)',
+          tooltip: 'var(--chat-tooltip)',
         },
       },
       boxShadow: {
-        card: "var(--chat-shadow-card)",
-        1: "0px 4px 8px -8px rgba(31, 35, 41, 0.06), 0px 6px 12px 0px rgba(31, 35, 41, 0.04), 0px 8px 24px 8px rgba(31, 35, 41, 0.04)",
-        "light-menu":
-          "0px 4px 8px 0px rgba(31, 35, 41, 0.03), 0px 3px 6px -6px rgba(31, 35, 41, 0.05), 0px 6px 18px 6px rgba(31, 35, 41, 0.03)",
+        card: 'var(--chat-shadow-card)',
+        1: '0px 4px 8px -8px rgba(31, 35, 41, 0.06), 0px 6px 12px 0px rgba(31, 35, 41, 0.04), 0px 8px 24px 8px rgba(31, 35, 41, 0.04)',
+        'light-menu':
+          '0px 4px 8px 0px rgba(31, 35, 41, 0.03), 0px 3px 6px -6px rgba(31, 35, 41, 0.05), 0px 6px 18px 6px rgba(31, 35, 41, 0.03)',
       },
     },
   },
--- tests/visual/spec/Chat.tsx
+++ tests/visual/spec/Chat.tsx
@@ -1,9 +1,9 @@
-import Chat from "@appflowy-chat/components/chat/index";
-import { Switch } from "@mui/material";
-import { ChangeEvent, useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import "../styles/variables/dark.variables.css";
-import "../styles/variables/light.variables.css";
+import Chat from '@appflowy-chat/components/chat/index';
+import { Switch } from '@mui/material';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import '../styles/variables/dark.variables.css';
+import '../styles/variables/light.variables.css';
 const ChatTest = () => {
   const [searchParams] = useSearchParams();
 
@@ -19,20 +19,20 @@ const ChatTest = () => {
   }
 
   useEffect(() => {
-    const isDark = searchParams.get("dark");
-    if (isDark === "true") {
+    const isDark = searchParams.get('dark');
+    if (isDark === 'true') {
       handleChangeMode(true);
     }
     return () => {};
   }, [searchParams]);
 
   return (
-    <div className="w-full h-full flex">
-      <div className="w-full max-w-[300px] p-4 border-r border-r-gray-500 max-h-full">
+    <div className='flex h-full w-full'>
+      <div className='max-h-full w-full max-w-[300px] border-r border-r-gray-500 p-4'>
         Sidebar
-        <hr className="mt-1" />
-        <div className="py-2">
-          <span>Theme mode: {isDarkMode ? "Dark" : "Light"}</span>
+        <hr className='mt-1' />
+        <div className='py-2'>
+          <span>Theme mode: {isDarkMode ? 'Dark' : 'Light'}</span>
           <div>
             <Switch checked={isDarkMode} onChange={handleChangeSwitchMode} />
           </div>
@@ -40,7 +40,7 @@ const ChatTest = () => {
       </div>
       <Chat
         userAvatar={
-          "https://www.strasys.uk/wp-content/uploads/2022/02/Depositphotos_484354208_S-768x768.jpg"
+          'https://www.strasys.uk/wp-content/uploads/2022/02/Depositphotos_484354208_S-768x768.jpg'
         }
       />
     </div>
