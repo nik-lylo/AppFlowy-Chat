658cbb2d5c81435e4ba9217e5e76ee8cebf072bb
Nick Lylo <lulnuk1995@gmail.com>
2024-12-18 23:17:21 +0100

    feat: integ appflowy editor

--- lib/components/message-response-content/index.tsx
+++ lib/components/message-response-content/index.tsx
@@ -1,11 +1,31 @@
-import { FC } from "react";
+import { Editor, NodeType } from '@appflowyinc/editor';
+import { FC } from 'react';
 
 interface IProps {
   body: string;
 }
 
 const MessageResponseContent: FC<IProps> = ({ body }) => {
-  return <div className="text-sm">{body}</div>;
+  return (
+    <div className='text-sm'>
+      <Editor
+        theme={'dark'}
+        readOnly
+        initialValue={[
+          {
+            type: NodeType.Paragraph,
+            data: { level: 1 },
+            delta: [
+              {
+                insert: body,
+              },
+            ],
+            children: [],
+          },
+        ]}
+      />
+    </div>
+  );
 };
 
 export default MessageResponseContent;
